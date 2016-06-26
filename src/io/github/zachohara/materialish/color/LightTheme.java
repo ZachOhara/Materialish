e5a03f6b5acf25a17ea35335f9770720a77b18a8
Zach Ohara <zjohara@gmail.com>
2016-06-26 16:03:01 -0700

    Add color utils

---
+++ src/io/github/zachohara/materialish/color/LightTheme.java
@@ -0,0 +1,30 @@
+/* Copyright (C) 2016 Zach Ohara
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package io.github.zachohara.materialish.color;
+
+public class LightTheme {
+	
+	public static final MaterialColor STATUS_BAR = MaterialColor.GREY_300;
+	public static final MaterialColor APP_BAR = MaterialColor.GREY_100;
+	public static final MaterialColor BACKGROUND = MaterialColor.GREY_050;
+	public static final MaterialColor DIALOGUE = MaterialColor.WHITE;
+	
+	private LightTheme() {
+		// take no action;
+	}
+	
+}
